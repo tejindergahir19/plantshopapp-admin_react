c9ed446c26105c7ddd7c4d22bf4eb39968b22249
Tejinder Singh <tsingh@mafcarrefour.com>
2023-06-02 15:10:12 +0530

    rating fix

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -246,11 +246,7 @@ console.clear()
                                                     )
                                             ))
                                         }
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
+                                       
                                     </select>
                                 </div>
 
